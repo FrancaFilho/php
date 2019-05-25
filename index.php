51861e316e6702763ecb57b4a38cece7f825820f
Milton França Filho <miltondsw@gmail.com>
2019-05-25 16:09:02 -0300

    criancao do repositorio

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Milton Ninja</title>
+</head>
+<body>
+    <?php
+        echo "Alô Milton França Filho, programando em PHP 7.0!";
+    ?>
+</body>
+</html>
